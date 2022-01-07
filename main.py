a9453b0ec9db54e6c049b47d3a40972e944c8347
LoseThos <88303617+sostratosaegina@users.noreply.github.com>
2022-01-07 00:47:18 +0000

    added loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-calculator = input('''
+def math():
+  calculator = input('''
 Type in the math operation you'd like you to perform:
 + for addition
 - for subtraction
@@ -6,24 +7,41 @@ Type in the math operation you'd like you to perform:
 * for multiplication
 ''')
 
-n1 = int(input('First Number: '))
-n2 = int(input('Second Number: '))
+  n1 = int(input('First Number: '))
+  n2 = int(input('Second Number: '))
 
-if calculator == '+':
-  print('{} + {} = ' .format(n1, n2))
-  print(n1 + n2)
+  if calculator == '+':
+    print('{} + {} = ' .format(n1, n2))
+    print(n1 + n2)
 
-elif calculator == '-':
-  print('{} - {} = ' .format(n1, n2))
-  print(n1 + n2)
+  elif calculator == '-':
+    print('{} - {} = ' .format(n1, n2))
+    print(n1 + n2)
 
-elif calculator == '*':
-  print('{} * {} = ' .format(n1, n2))
-  print(n1 + n2)
+  elif calculator == '*':
+    print('{} * {} = ' .format(n1, n2))
+    print(n1 + n2)
 
-elif calculator == '/':
-  print('{} / {} = ' .format(n1, n2))
-  print(n1 + n2)
+  elif calculator == '/':
+    print('{} / {} = ' .format(n1, n2))
+    print(n1 + n2)
 
-else:
-  print('you dun goofed')
+  else:
+    print('you dun goofed')
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        math()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+math()
+
+again()
